70aaa22e56f79eb070eabe59c174ee4cf957f4a4
Anastasios <apantzar@csd.auth.gr>
2021-03-25 20:06:40 +0000

    API changed

--- Maps/marker.js
+++ Maps/marker.js
@@ -27,7 +27,7 @@
                    
                 })
     
-                L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=tmIAY714HkSJ3YTHhuQE',{
+                L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=2spIdQjJtUxW69tie1Xh',{
                     minZoom: 2, //fixed zoom (min zoom)
                     maxZoom: 5,
                     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
