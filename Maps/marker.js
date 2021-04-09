e63d819b3991ceb7a3891de341eb04065e27a1f0
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-04-09 17:24:52 +0300

    Ship icon added, footer added

--- Maps/marker.js
+++ Maps/marker.js
@@ -24,6 +24,19 @@
                     iconSize:     [40, 40], // size of the icon
                    
                 })
+
+
+
+
+
+                //=====SHIP======
+                
+                const ship = L.icon({
+    
+                  iconUrl: '../Maps/icons/ship.png',
+                  iconSize:     [40, 40], // size of the icon
+                 
+              })
     
                 L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=2spIdQjJtUxW69tie1Xh',{
                     minZoom: 2, //fixed zoom (min zoom)
@@ -39,58 +52,54 @@
                 //======================================================================multiple-marks (blue)=================================================================================== 
                 const packetMarks = L.layerGroup([
                     
-                    new L.marker([13.083333, -55.466667]).bindPopup(`<h2>Atlantic Empress</h2>`).on('click', function(ev) {// ev is an event object (MouseEvent in this case)
+                    new L.marker([13.083333, -55.466667],{icon: ship}).bindPopup(`<h2>Atlantic Empress</h2>`).on('click', function(ev) {// ev is an event object (MouseEvent in this case)
                     
                    
                         changeTextWithMarker(atlantic , 50.083333, -55.466667 );
-
-                       
-                   
                   
-                  
-                  console.log();
+                 
                     }),//Atlantic Empress
     
-                    new L.marker([-12.027189, 12.230525]).bindPopup(`<h2>ABT Summer</h2>`).on('click', function(ev){
+                    new L.marker([-12.027189, 12.230525],{icon: ship}).bindPopup(`<h2>ABT Summer</h2>`).on('click', function(ev){
                       changeTextWithMarker(abtSummer, 30.027189 ,  12.230525);
                      
                     }),//ABT Summer
                     
                     
-                    new L.marker([-33.275833, 17.504667]).bindPopup(`<h2>Castillo de Bellver</h2>`).on('click', function(ev){
+                    new L.marker([-33.275833, 17.504667] ,{icon: ship}).bindPopup(`<h2>Castillo de Bellver</h2>`).on('click', function(ev){
                       changeTextWithMarker(castilloDeBellvre, 5.275833, 17.504667)
                   }),//Castillo de Bellver
-                    new L.marker([48.6,-4.7]).bindPopup(`<h2>Amoco Cadiz</h2`).on('click', function(ev){
+                    new L.marker([48.6,-4.7],{icon: ship}).bindPopup(`<h2>Amoco Cadiz</h2`).on('click', function(ev){
                       changeTextWithMarker(amocoCadiz,76.6,-4.7)
                   }),           //Amoco Cadiz
-                    new L.marker([44.366750, 8.700028]).bindPopup(`<h2>Haven</h2`).on('click', function(ev){
+                    new L.marker([44.366750, 8.700028],{icon: ship}).bindPopup(`<h2>Haven</h2`).on('click', function(ev){
                       changeTextWithMarker(haven , 75.366750, 8.700028)
                   }), //Haven
-                    new L.marker([25.3, 57.566667]).bindPopup(`<h2>Sea Star</h2`).on('click', function(ev){
+                    new L.marker([25.3, 57.566667],{icon: ship}).bindPopup(`<h2>Sea Star</h2`).on('click', function(ev){
                       changeTextWithMarker(seaStar,58.3, 57.566667)
                   }),    //Sea Star   
-                    new L.marker([28.366700, 125.916700]).bindPopup(`<h2>Sanchi</h2`).on('click', function(ev){
+                    new L.marker([28.366700, 125.916700],{icon: ship}).bindPopup(`<h2>Sanchi</h2`).on('click', function(ev){
                       changeTextWithMarker(sanchi, 62.366700, 125.916700)
                   }),     //SANCHI           
-                    new L.marker([43.369462, -8.381184]).bindPopup(`<h2>Urquiola</h2>`).on('click', function(ev){
+                    new L.marker([43.369462, -8.381184],{icon: ship}).bindPopup(`<h2>Urquiola</h2>`).on('click', function(ev){
                       changeTextWithMarker(urquiola,75.369462, -8.381184)
                   }),   //Urquiola    
-                    new L.marker([51.638665, -40.486299]).bindPopup(`<h2>Odyssey</h2>`).on('click', function(ev){
+                    new L.marker([51.638665, -40.486299],{icon: ship}).bindPopup(`<h2>Odyssey</h2>`).on('click', function(ev){
                       changeTextWithMarker(odyssey,75.638665, -40.486299 )
                   }), //Odyssey
-                    new L.marker([21.201243, -162.895969]).bindPopup(`<h2>Hawaiian Patriot</h2>`).on('click', function(ev){
+                    new L.marker([21.201243, -162.895969],{icon: ship}).bindPopup(`<h2>Hawaiian Patriot</h2>`).on('click', function(ev){
                       changeTextWithMarker(hawaiianPatriot, 57.201243, -162.895969)
                   }), //Hawaiian Patriot  
-                    new L.marker([40.994167, 29.007778]).bindPopup(`<h2>Independenta</h2`).on('click', function(ev){
+                    new L.marker([40.994167, 29.007778],{icon: ship}).bindPopup(`<h2>Independenta</h2`).on('click', function(ev){
                       changeTextWithMarker(independenta , 72.994167, 29.007778)
                   }), //Independenta
-                    new L.marker([41.180765, -8.699863]).bindPopup(`<h2>Jakob Maersk</h2>`).on('click', function(ev){
+                    new L.marker([41.180765, -8.699863],{icon: ship}).bindPopup(`<h2>Jakob Maersk</h2>`).on('click', function(ev){
                       changeTextWithMarker(jakobMaersk, 75.180765, -8.699863 )
                   }), //Jakob Maersk
-                    new L.marker([59.883333, -1.35]).bindPopup(`<h2>Braer</h2>`).on('click', function(ev){
+                    new L.marker([59.883333, -1.35],{icon: ship}).bindPopup(`<h2>Braer</h2>`).on('click', function(ev){
                       changeTextWithMarker(braer,75.883333, -1.35)
                   }), //Braer
-                    new L.marker([43.389, -8.41]).bindPopup(`<h2>Aegean Sea</h2>`).on('click', function(ev){
+                    new L.marker([43.389, -8.41],{icon: ship}).bindPopup(`<h2>Aegean Sea</h2>`).on('click', function(ev){
                       changeTextWithMarker(aegeanSea,75.389, -8.41)
                   }), //Aegean Sea   
                     
@@ -142,6 +151,8 @@
                 console.log(popText);
 
 
+
+
     
          //Function to change html file via click
          var text1="" ;
@@ -172,13 +183,14 @@
                 document.getElementById("htmlPath").src = "../Maps/text.html"; //get the file
               
                 document.getElementById("htmlPath").style= "position:absolute; top: 100%; bottom: 0%; left: 0%; right: 50%; border:none; ";
+                document.getElementById("footer").style="position: absolute; top:150%";
 
                
                
            
                 
                 //Auto scroll the window
-                window.scrollTo(300, 700);
+                window.scrollTo(300, 490);
             
             }
 
