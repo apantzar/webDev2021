7247db7af2770bbf20a5871f886f0158d1f6fc75
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-06-14 14:44:55 +0300

    Dashboard UI (dEMO)

--- admin/adminPage.php
+++ admin/adminPage.php
@@ -28,13 +28,16 @@
 
 
  
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="admin/style.css">
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-     integrity="sha384-wvfXpqpZZVQGK6TAh5PV1GOfQNHSoD2xbE+QkPxCAF1NEevoEH3S10sibVcOQVnN" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="style.css">
+     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" -->
+     <!-- integrity="sha384-wvfXpqpZZVQGK6TAh5PV1GOfQNHSoD2xbE+QkPxCAF1NEevoEH3S10sibVcOQVnN" crossorigin="anonymous">  -->
+     <!-- <link rel="stylesheet" href="sb-admin-2.css"> -->
+     <!-- <link rel="stylesheet" href="sb-admin-2.min.css">  -->
 
 </head>
 <body>
@@ -130,16 +133,90 @@
             
             
 
-            <!-- <div class="cardBox">
-                <div class="card">
-                    <div>
-                        <div class="numbers">1,042</div>
-                        <div class="cardName">Total Users</div>
-                    </div>
-                </div>
+            <div class="row">
 
-            </div> -->
-           
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card border-left-primary shadow h-100 py-2">
+    <div class="card-body">
+      <div class="row no-gutters align-items-center">
+        <div class="col mr-2">
+          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Registered Admin</div>
+          <div class="h5 mb-0 font-weight-bold text-gray-800">
+
+           <h4>Total Admin: *</h4>
+
+          </div>
+        </div>
+        <div class="col-auto">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card border-left-success shadow h-100 py-2">
+    <div class="card-body">
+      <div class="row no-gutters align-items-center">
+        <div class="col mr-2">
+          <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+          <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+        </div>
+        <div class="col-auto">
+        
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card border-left-info shadow h-100 py-2">
+    <div class="card-body">
+      <div class="row no-gutters align-items-center">
+        <div class="col mr-2">
+          <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+          <div class="row no-gutters align-items-center">
+            <div class="col-auto">
+              <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+            </div>
+            <div class="col">
+              <div class="progress progress-sm mr-2">
+                <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50"
+                  aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-auto">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Pending Requests Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card border-left-warning shadow h-100 py-2">
+    <div class="card-body">
+      <div class="row no-gutters align-items-center">
+        <div class="col mr-2">
+          <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+          <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+        </div>
+        <div class="col-auto">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
            
 
