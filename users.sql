82183679ae81c55637b0d8c103f5ef442f372787
Thomas Tagaras <ttagaras@csd.auth.gr>
2021-06-15 00:22:16 +0300

    remeles

--- users.sql
+++ users.sql
@@ -44,7 +44,11 @@ CREATE TABLE users (
   userType varchar(100) NOT NULL DEFAULT 'user'
 );
 
-
+CREATE TABLE profileimg(
+  id int(11) not null PRIMARY KEY AUTO_INCREMENT,
+  userid int(11) not null,
+  status int(11) not null
+);
 
 
 
